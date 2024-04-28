2290b866b3288e2a3e8fc701ce12e014eefa4f81
Raúl Sánchez Mondéjar <rsanchez@refineriaweb.com>
2024-04-28 22:49:37 +0200

    ADD NavBar component

---
+++ components/NavBar.tsx
@@ -0,0 +1,203 @@
+"use client";
+
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import WidgetsIcon from '@mui/icons-material/Widgets';
+import Link from "next/link";
+import {useSession} from "next-auth/react";
+
+const pages: {title: string, url: string}[] = [
+    {title: 'Dashboard', url: '/dashboard'},
+    {title: 'Nuevo Formulario', url: '/forms/create'}
+];
+
+const settings: {title: string, url: string}[] = [
+    {title: 'Cuenta', url: '#cuenta'},
+    {title: 'Cerrar sesión', url: '#sign-out'}
+];
+
+export default function NavBar() {
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+    const { data: session} = useSession({
+        required: false,
+    });
+
+    const userAcronyms: string = (session?.user?.name?.charAt(0).toUpperCase() || 'U') + (session?.user?.lastName?.charAt(0).toUpperCase() || 'U');
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <WidgetsIcon sx={{display: {xs: 'none', md: 'flex'}, mr: 1}}/>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component={Link}
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: {xs: 'none', md: 'flex'},
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        Dynamic Blocks
+                    </Typography>
+
+                    <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: {xs: 'block', md: 'none'},
+                            }}
+                        >
+                            {session && (
+                                pages.map((page: {title: string, url: string}) => (
+                                    <MenuItem
+                                        key={`menu_a_${page.url}`}
+                                        href={page.url}
+                                        component={Link}
+                                    >
+                                        <Typography textAlign="center">{page.title}</Typography>
+                                    </MenuItem>
+                                ))
+                            ) || (
+                                <MenuItem
+                                    href="/api/auth/signin"
+                                    component={Link}
+                                >
+                                    <Typography textAlign="center">Iniciar sesión</Typography>
+                                </MenuItem>
+                            )}
+                        </Menu>
+                    </Box>
+                    <WidgetsIcon sx={{display: {xs: 'flex', md: 'none'}, mr: 1}}/>
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href="#app-bar-with-responsive-menu"
+                        sx={{
+                            mr: 2,
+                            display: {xs: 'flex', md: 'none'},
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        Dynamic Blocks
+                    </Typography>
+                    <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
+                        {session && (
+                            pages.map((page: {title: string, url: string}) => (
+                                <Button
+                                    key={`menu_b_${page.url}`}
+                                    href={page.url}
+                                    component={Link}
+                                    sx={{my: 2, color: 'white', display: 'block'}}
+                                >
+                                    {page.title}
+                                </Button>
+                            ))
+                        )}
+                    </Box>
+
+                    {session && (
+                        <Box sx={{flexGrow: 0}}>
+                            <Tooltip title="Abrir configuración">
+                                <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
+                                    <Avatar alt="User Avatar">
+                                        {userAcronyms}
+                                    </Avatar>
+                                </IconButton>
+                            </Tooltip>
+                            <Menu
+                                sx={{mt: '45px'}}
+                                id="menu-appbar"
+                                anchorEl={anchorElUser}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={Boolean(anchorElUser)}
+                                onClose={handleCloseUserMenu}
+                            >
+                                {settings.map((setting: {title: string, url: string}) => (
+                                    <MenuItem
+                                        key={`menu_c_${setting.url}`}
+                                        href={setting.url}
+                                        component={Link}
+                                        onClick={handleCloseUserMenu}
+                                    >
+                                        <Typography textAlign="center">{setting.title}</Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+                    )}
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
