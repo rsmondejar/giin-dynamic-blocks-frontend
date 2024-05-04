bfeeb3555433c418a67ae0fba2a3a0ddef34e21d
Raúl Sánchez Mondéjar <rsanchez@refineriaweb.com>
2024-05-04 22:00:20 +0200

    ADD FormCreateSpeedDial component. Not finished yet

---
+++ components/forms/FormCreateSpeedDial.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import SaveIcon from '@mui/icons-material/Save';
+import PrintIcon from '@mui/icons-material/Print';
+import AddCommentIcon from '@mui/icons-material/AddComment';
+
+const actions = [
+    { icon: <SaveIcon />, name: 'Save' },
+    { icon: <PrintIcon />, name: 'Print' },
+    { icon: <AddCommentIcon />, name: 'Añadir pregunta' },
+];
+
+export default function FormCreateSpeedDial() {
+    return (
+        <Box sx={{ height: 320, transform: 'translateZ(0px)', flexGrow: 1 }}>
+            <SpeedDial
+                ariaLabel="SpeedDial basic example"
+                sx={{ position: 'absolute', bottom: 16, right: 16 }}
+                icon={<SpeedDialIcon />}
+            >
+                {actions.map((action) => (
+                    <SpeedDialAction
+                        key={action.name}
+                        icon={action.icon}
+                        tooltipTitle={action.name}
+                    />
+                ))}
+            </SpeedDial>
+        </Box>
+    );
+}
