e60c64656623f80ac75f7caa1f4e9b7ab3feee73
Raúl Sánchez Mondéjar <rsanchez@refineriaweb.com>
2024-05-14 21:53:30 +0200

    FIX Loading screen

--- components/LoadingBackdrop.tsx
+++ components/LoadingBackdrop.tsx
@@ -2,21 +2,27 @@
 import * as React from 'react';
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
+import {useEffect} from "react";
 
 export default function LoadingBackdrop(
     propsIn: Readonly<{
-        open?: boolean,
+        open: boolean,
         allowToClose?: boolean,
     }>
 ) {
-    const [open, setOpen] = React.useState(propsIn.open || false);
-    const [allowToClose] = React.useState(propsIn.open || false);
+    const [open, setOpen]: [boolean, (value: (((prevState: boolean) => boolean) | boolean)) => void] = React.useState(propsIn.open);
+    const [allowToClose, setAllowToClose]: [boolean, (value: (((prevState: boolean) => boolean) | boolean)) => void] = React.useState(propsIn.allowToClose || false);
+
+    useEffect(() => {
+        setOpen(propsIn.open);
+        setAllowToClose(propsIn.allowToClose || false);
+    }, [propsIn]);
+
     const handleClose = () => {
         if (allowToClose) {
             setOpen(false);
         }
     };
-
 
     return (
         <div>
