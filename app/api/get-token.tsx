2c271f5430ab76c3730ba9a2e9dc72f8273afbc9
Raúl Sánchez Mondéjar <rsanchez@refineriaweb.com>
2024-05-24 23:10:11 +0200

    Fix vercel

--- app/api/get-token.tsx
+++ app/api/get-token.tsx
@@ -1,5 +1,6 @@
 import {getServerSession} from "next-auth";
-import {authOptions} from "@/app/api/auth/[...nextauth]/route";
+import {authOptions} from "@/lib/authOptions";
+
 
 export const getUserToken = async () => {
     const session = await getServerSession(authOptions);
