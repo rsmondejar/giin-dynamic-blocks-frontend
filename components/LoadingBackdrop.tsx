d429deea0c2ac10c9eec781fff42d9c372f70c9f
Raúl Sánchez Mondéjar <rsanchez@refineriaweb.com>
2024-05-24 23:13:14 +0200

    Fix loading dependency

--- components/LoadingBackdrop.tsx
+++ components/LoadingBackdrop.tsx
@@ -6,7 +6,7 @@ import {useEffect} from "react";
 
 export default function LoadingBackdrop(
     propsIn: Readonly<{
-        open: boolean,
+        open?: boolean,
         allowToClose?: boolean,
     }>
 ) {
