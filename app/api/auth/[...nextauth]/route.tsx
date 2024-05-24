db51f1f77ed5d5cc933a8893feb5d61c6ca76721
Raúl Sánchez Mondéjar <rsanchez@refineriaweb.com>
2024-05-24 23:06:38 +0200

    Fix vercel

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -2,4 +2,4 @@ import NextAuth from "next-auth"
 import {authOptions} from "@/lib/authOptions";
 
 export const handler = NextAuth(authOptions) as never;
-export { handler as GET, handler as POST, authOptions }
+export { handler as GET, handler as POST }
